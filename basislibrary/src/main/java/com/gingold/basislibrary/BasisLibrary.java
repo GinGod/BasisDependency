70c5b1c133919b83450226cf7167e3d8f1775def
Gingold <decetwen1989@163.com>
2017-07-26 13:45:29 +0800

    添加okhttp3.8.1

--- basislibrary/src/main/java/com/gingold/basislibrary/BasisLibrary.java
+++ basislibrary/src/main/java/com/gingold/basislibrary/BasisLibrary.java
@@ -1,9 +1,13 @@
 package com.gingold.basislibrary;
 
+import okhttp3.OkHttpClient;
+
 /**
  *
  */
 
 public class BasisLibrary {
-
+    public static void method() {
+        OkHttpClient client = new OkHttpClient();
+    }
 }
