d57f00426a934f0961703fc6529cc9e20a90b940
Gingod <gingod@126.com>
2017-08-15 13:41:25 +0800

    更改编译版本至 26.0.0

--- basislibrary/src/main/java/com/gingold/basislibrary/view/GridViewWithMaxH.java
+++ basislibrary/src/main/java/com/gingold/basislibrary/view/GridViewWithMaxH.java
@@ -2,6 +2,7 @@ package com.gingold.basislibrary.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.GridView;
 
 /**
@@ -26,5 +27,13 @@ public class GridViewWithMaxH extends GridView {
         int expandSpec = MeasureSpec.makeMeasureSpec(
                 Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
         super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if(ev.getAction() == MotionEvent.ACTION_MOVE){
+            return true;//true:禁止滚动
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
